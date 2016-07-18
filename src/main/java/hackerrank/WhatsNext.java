70c8f0b8dffbe1b4b5bdf3f23d7e3ad0ade7c746
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-18 01:35:53 -0500

    Update What's Next

--- src/main/java/hackerrank/WhatsNext.java
+++ src/main/java/hackerrank/WhatsNext.java
@@ -6,12 +6,39 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/whats-next
  */
 public class WhatsNext {
+    private static long compress(String num) {
+        long x = 0;
+        for (int i = 0; i < num.length(); i++) {
+            int n = Integer.parseInt("" + num.charAt(i));
+            if (i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    x <<= 1;
+                    x |= 1;
+                }
+            } else {
+                for (int j = 0; j < n; j++) {
+                    x <<= 1;
+                }
+            }
+        }
+        return x;
+    }
+
+    private static void whatsNext(String num) {
+        String compressed = Long.toBinaryString(compress(num));
+        // TODO
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int testCases = in.nextInt();
         for (int t = 0; t < testCases; t++) {
             int length = in.nextInt();
-            int num = in.nextInt();
+            String num = "";
+            for (int i = 0; i < length; i++) {
+                num += in.next();
+            }
+            whatsNext(num);
         }
     }
 }
--- src/test/java/hackerrank/WhatsNextTest.java
+++ src/test/java/hackerrank/WhatsNextTest.java
@@ -10,4 +10,18 @@ public class WhatsNextTest {
         System.setIn(new FileInputStream("src/test/resources/whats-next/test1.txt"));
         WhatsNext.main(new String[]{});
     }
+
+    @Test
+    public void test() {
+        int a = 0;
+        for (int i = 0; i < 5; i++) {
+            a <<= 1;
+            a |= 1;
+        }
+        System.out.println(Integer.toBinaryString(a));
+        for (int i = 0; i < 3; i++) {
+            a <<= 1;
+        }
+        System.out.println(Integer.toBinaryString(a));
+    }
 }
