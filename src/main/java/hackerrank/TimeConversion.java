a3e0c94442376145e4d855eeaebeaf89279ce3c8
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-18 00:40:14 -0500

    Solve Time Conversion

--- src/main/java/hackerrank/TimeConversion.java
+++ src/main/java/hackerrank/TimeConversion.java
@@ -1,10 +1,28 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/time-conversion
  */
 public class TimeConversion {
-    public static void main(String[] args) {
+    private static void convert(String str) {
+        String[] split = str.split(":");
+        int hour = Integer.parseInt(split[0]);
+        int minute = Integer.parseInt(split[1]);
+        int second = Integer.parseInt(split[2].substring(0, 2));
+        String ampm = split[2].substring(2);
+        if (ampm.equals("PM") && hour < 12) {
+            hour += 12;
+        } else if (ampm.equals("AM") && hour == 12) {
+            hour = 0;
+        }
+        System.out.printf("%02d:%02d:%02d\n", hour, minute, second);
+    }
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        convert(str);
     }
 }
--- src/test/java/hackerrank/TimeConversionTest.java
+++ src/test/java/hackerrank/TimeConversionTest.java
@@ -10,4 +10,22 @@ public class TimeConversionTest {
         System.setIn(new FileInputStream("src/test/resources/time-conversion/test1.txt"));
         TimeConversion.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/time-conversion/test2.txt"));
+        TimeConversion.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/time-conversion/test3.txt"));
+        TimeConversion.main(new String[]{});
+    }
+
+    @Test
+    public void test4() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/time-conversion/test4.txt"));
+        TimeConversion.main(new String[]{});
+    }
 }
