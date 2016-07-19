b97891d6fd4504c9e10033ac342cf81a3f6dfc54
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-19 14:58:26 -0500

    Update Equals Stack problem

--- src/main/java/hackerrank/EqualsStack.java
+++ src/main/java/hackerrank/EqualsStack.java
@@ -8,5 +8,20 @@ import java.util.Scanner;
 public class EqualsStack {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int n1 = in.nextInt();
+        int n2 = in.nextInt();
+        int n3 = in.nextInt();
+        int h1[] = new int[n1];
+        for (int h1_i = 0; h1_i < n1; h1_i++) {
+            h1[h1_i] = in.nextInt();
+        }
+        int h2[] = new int[n2];
+        for (int h2_i = 0; h2_i < n2; h2_i++) {
+            h2[h2_i] = in.nextInt();
+        }
+        int h3[] = new int[n3];
+        for (int h3_i = 0; h3_i < n3; h3_i++) {
+            h3[h3_i] = in.nextInt();
+        }
     }
 }
