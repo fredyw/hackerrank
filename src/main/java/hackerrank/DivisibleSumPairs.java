8744220d93d45c2f06801f12efbd73e44ebed81e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-19 11:16:18 -0500

    Solve Divisible Sum Pairs

--- src/main/java/hackerrank/DivisibleSumPairs.java
+++ src/main/java/hackerrank/DivisibleSumPairs.java
@@ -6,6 +6,18 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/divisible-sum-pairs
  */
 public class DivisibleSumPairs {
+    private static int divisibleSumParis(int[] a, int k) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if ((a[i] + a[j]) % k == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -14,5 +26,6 @@ public class DivisibleSumPairs {
         for (int a_i = 0; a_i < n; a_i++) {
             a[a_i] = in.nextInt();
         }
+        System.out.println(divisibleSumParis(a, k));
     }
 }
