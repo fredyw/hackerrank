11a969a8c7f78f8b6479d281ba9c8a0787775024
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-19 14:12:07 -0500

    Solve Angry Professor problem

--- src/main/java/hackerrank/AngryProfessor.java
+++ src/main/java/hackerrank/AngryProfessor.java
@@ -1,10 +1,29 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/angry-professor
  */
 public class AngryProfessor {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int onTime = 0;
+            for (int i = 0; i < n; i++) {
+                int a = in.nextInt();
+                if (a <= 0) {
+                    onTime++;
+                }
+            }
+            if (onTime >= k) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
     }
 }
