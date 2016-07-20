2fe95e44dfbcb05771e60e7cba94e7ded5c01703
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-20 11:46:31 -0500

    Solve Mars Exploration problem

--- src/main/java/hackerrank/MarsExploration.java
+++ src/main/java/hackerrank/MarsExploration.java
@@ -9,5 +9,18 @@ public class MarsExploration {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.next();
+        int count = 0;
+        char[] word = new char[]{'S', 'O', 'S'};
+        int idx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx == word.length) {
+                idx = 0;
+            }
+            if (word[idx] != s.charAt(i)) {
+                count++;
+            }
+            idx++;
+        }
+        System.out.println(count);
     }
 }
