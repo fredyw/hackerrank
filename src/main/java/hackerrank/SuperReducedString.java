f5b14cd50155fb1ae8268f7bf1ad7f881b9405a2
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-24 14:52:37 -0500

    Solve Super Reduced String

--- src/main/java/hackerrank/SuperReducedString.java
+++ src/main/java/hackerrank/SuperReducedString.java
@@ -6,7 +6,32 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/reduced-string
  */
 public class SuperReducedString {
+    private static void superReducedString(String str) {
+        boolean foundPairs = false;
+        String newString = str;
+        for (int i = 1; i < newString.length(); i++) {
+            if (newString.charAt(i - 1) == newString.charAt(i)) {
+                foundPairs = true;
+                newString = newString.substring(0, i - 1) +
+                    newString.substring(i + 1);
+            }
+        }
+        while (foundPairs) {
+            foundPairs = false;
+            for (int i = 1; i < newString.length(); i++) {
+                if (newString.charAt(i - 1) == newString.charAt(i)) {
+                    foundPairs = true;
+                    newString = newString.substring(0, i - 1) +
+                        newString.substring(i + 1);
+                }
+            }
+        }
+        System.out.println(newString.isEmpty() ? "Empty String" : newString);
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String str = in.next();
+        superReducedString(str);
     }
 }
--- src/test/java/hackerrank/SuperReducedStringTest.java
+++ src/test/java/hackerrank/SuperReducedStringTest.java
@@ -10,4 +10,34 @@ public class SuperReducedStringTest {
         System.setIn(new FileInputStream("src/test/resources/super-reduced-string/test1.txt"));
         SuperReducedString.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/super-reduced-string/test2.txt"));
+        SuperReducedString.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/super-reduced-string/test3.txt"));
+        SuperReducedString.main(new String[]{});
+    }
+
+    @Test
+    public void test4() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/super-reduced-string/test4.txt"));
+        SuperReducedString.main(new String[]{});
+    }
+
+    @Test
+    public void test5() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/super-reduced-string/test5.txt"));
+        SuperReducedString.main(new String[]{});
+    }
+
+    @Test
+    public void test6() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/super-reduced-string/test6.txt"));
+        SuperReducedString.main(new String[]{});
+    }
 }
