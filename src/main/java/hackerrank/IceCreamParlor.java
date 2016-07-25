238b5df1d4a7162947c770374df1776e0f1c7850
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-24 23:33:56 -0500

    Solve Ice Cream Parlor

--- src/main/java/hackerrank/IceCreamParlor.java
+++ src/main/java/hackerrank/IceCreamParlor.java
@@ -1,5 +1,9 @@
 package hackerrank;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -8,5 +12,38 @@ import java.util.Scanner;
 public class IceCreamParlor {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int m = in.nextInt();
+            int n = in.nextInt();
+            int[] array = new int[n];
+            Map<Integer, List<Integer>> map = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                int p = in.nextInt();
+                array[i] = p;
+                if (!map.containsKey(p)) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(i);
+                    map.put(p, list);
+                } else {
+                    map.get(p).add(i);
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                int p1 = array[i];
+                int p2 = m - p1;
+                if (map.containsKey(p2)) {
+                    List<Integer> list = map.get(p2);
+                    if (p1 == p2 && list.size() >= 2) {
+                        System.out.println((i + 1) + " " + (list.get(1) + 1));
+                        break;
+                    } else if (p1 != p2) {
+                        System.out.println((i + 1) + " " + (list.get(0) + 1));
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
--- src/test/java/hackerrank/IceCreamParlorTest.java
+++ src/test/java/hackerrank/IceCreamParlorTest.java
@@ -10,4 +10,16 @@ public class IceCreamParlorTest {
         System.setIn(new FileInputStream("src/test/resources/ice-cream-parlor/test1.txt"));
         IceCreamParlor.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/ice-cream-parlor/test2.txt"));
+        IceCreamParlor.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/ice-cream-parlor/test3.txt"));
+        IceCreamParlor.main(new String[]{});
+    }
 }
