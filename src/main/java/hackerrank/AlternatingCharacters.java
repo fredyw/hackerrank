6779d4339203da5be4d561529c2dc7873a67704d
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-26 00:28:19 -0500

    Solve Alternating Characters

--- src/main/java/hackerrank/AlternatingCharacters.java
+++ src/main/java/hackerrank/AlternatingCharacters.java
@@ -6,7 +6,24 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/alternating-characters
  */
 public class AlternatingCharacters {
+    private static int alternatingChars(String str) {
+        char[] chars = str.toCharArray();
+        int count = 0;
+        char prev = chars[0];
+        for (int i = 1; i < chars.length; i++) {
+            if (prev == chars[i]) {
+                count++;
+            }
+            prev = chars[i];
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            System.out.println(alternatingChars(in.next()));
+        }
     }
 }
