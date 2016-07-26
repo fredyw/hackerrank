c17b862ee47d308becf40a2f40b7922c2f1019a3
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-26 09:46:38 -0500

    Update Snakes and Ladders

--- src/main/java/hackerrank/SnakesAndLadders.java
+++ src/main/java/hackerrank/SnakesAndLadders.java
@@ -1,10 +1,25 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/the-quickest-way-up
  */
 public class SnakesAndLadders {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            for (int i = 0; i < n; i++) {
+                int from = in.nextInt();
+                int to = in.nextInt();
+            }
+            int m = in.nextInt();
+            for (int i = 0; i < m; i++) {
+                int from = in.nextInt();
+                int to = in.nextInt();
+            }
+        }
     }
 }
