4f2adf58e6e8a076d439e0cc69ec1accce8d6313
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-29 01:43:36 -0500

    Update Stock Maximize

--- src/main/java/hackerrank/StockMaximize.java
+++ src/main/java/hackerrank/StockMaximize.java
@@ -1,10 +1,26 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/stockmax
  */
 public class StockMaximize {
-    public static void main(String[] args) {
+    private static int maxProfit(int[] array) {
+        // TODO
+        return 0;
+    }
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++) {
+                array[i] = in.nextInt();
+            }
+            System.out.println(maxProfit(array));
+        }
     }
 }
