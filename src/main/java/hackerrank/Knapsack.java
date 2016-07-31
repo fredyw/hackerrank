b7c4105c2b8a90ee512998a44d8c9a2155bd95a9
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-30 21:42:06 -0500

    Skeleton for Knapsack

---
+++ src/main/java/hackerrank/Knapsack.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/unbounded-knapsack
+ */
+public class Knapsack {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // TODO
+    }
+}
