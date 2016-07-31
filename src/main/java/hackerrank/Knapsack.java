489826691c8cb43637a51a9c765d79674e88f45d
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-31 01:22:31 -0500

    Update Knapsack

--- src/main/java/hackerrank/Knapsack.java
+++ src/main/java/hackerrank/Knapsack.java
@@ -1,13 +1,48 @@
 package hackerrank;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
  * https://www.hackerrank.com/challenges/unbounded-knapsack
  */
 public class Knapsack {
+    private static int knapsack(int[] nums, int k) {
+        return knapsack(nums, k, 0, new HashMap<>());
+    }
+
+    private static int knapsack(int[] nums, int k, int prev, Map<Integer, Integer> memo) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int nextK = k - nums[i];
+            if (memo.containsKey(nextK)) {
+                max = Math.max(max, memo.get(nextK));
+            } else {
+                if (k - nums[i] == 0) {
+                    max = Math.max(max, nums[i] + prev);
+                } else if (k - nums[i] < 0) {
+                    max = Math.max(max, prev);
+                } else {
+                    max = Math.max(max, knapsack(nums, nextK, nums[i], memo) + prev);
+                }
+            }
+        }
+        memo.put(k, max);
+        return max;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = in.nextInt();
+            }
+            System.out.println(knapsack(nums, k));
+        }
     }
 }
--- src/test/java/hackerrank/KnapsackTest.java
+++ src/test/java/hackerrank/KnapsackTest.java
@@ -7,7 +7,13 @@ import java.io.FileInputStream;
 public class KnapsackTest {
     @Test
     public void test1() throws Exception {
-        System.setIn(new FileInputStream(""));
+        System.setIn(new FileInputStream("src/test/resources/knapsack/test1.txt"));
+        Knapsack.main(new String[]{});
+    }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/knapsack/test2.txt"));
         Knapsack.main(new String[]{});
     }
 }
