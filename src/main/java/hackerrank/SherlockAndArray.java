358834275614d0a2b46609283e9deb79509731ce
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-31 22:40:36 -0500

    Update Sherlock and Array

--- src/main/java/hackerrank/SherlockAndArray.java
+++ src/main/java/hackerrank/SherlockAndArray.java
@@ -6,8 +6,21 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/sherlock-and-array
  */
 public class SherlockAndArray {
+    private static String sherlockAndArray(int[] nums) {
+        // TODO
+        return "";
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = in.nextInt();
+            }
+            System.out.println(sherlockAndArray(nums));
+        }
     }
 }
--- src/test/java/hackerrank/SherlockAndArrayTest.java
+++ src/test/java/hackerrank/SherlockAndArrayTest.java
@@ -7,7 +7,7 @@ import java.io.FileInputStream;
 public class SherlockAndArrayTest {
     @Test
     public void test1() throws Exception {
-        System.setIn(new FileInputStream(""));
+        System.setIn(new FileInputStream("src/test/resources/sherlock-and-array/test1.txt"));
         SherlockAndArray.main(new String[]{});
     }
 }
