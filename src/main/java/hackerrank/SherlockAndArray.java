9d90da400776095a3dfff5f907c5bfb3771f6c68
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-07-31 22:37:03 -0500

    Skeleton for Sherlock and Array

---
+++ src/main/java/hackerrank/SherlockAndArray.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/sherlock-and-array
+ */
+public class SherlockAndArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
---
+++ src/test/java/hackerrank/SherlockAndArrayTest.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+
+public class SherlockAndArrayTest {
+    @Test
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream(""));
+        SherlockAndArray.main(new String[]{});
+    }
+}
