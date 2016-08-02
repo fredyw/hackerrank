a0efda3e11009dc4d8a99ce95ff8cf2842104cfa
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-01 23:45:05 -0500

    Update Priyanka and Toys

--- src/main/java/hackerrank/PriyankaAndToys.java
+++ src/main/java/hackerrank/PriyankaAndToys.java
@@ -1,10 +1,23 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/priyanka-and-toys
  */
 public class PriyankaAndToys {
-    public static void main(String[] args) {
+    private static int priyankAndToys(int[] weights) {
+        // TODO
+        return 0;
+    }
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] weights = new int[n];
+        for (int i = 0; i < n; i++) {
+            weights[i] = in.nextInt();
+        }
+        System.out.println(priyankAndToys(weights));
     }
 }
