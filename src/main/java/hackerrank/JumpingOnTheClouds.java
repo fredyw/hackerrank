b904a9b83d0b137d8da66ff6e3b9f28c85248d22
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-03 23:47:05 -0500

    Solve Jumping on the Clouds

--- src/main/java/hackerrank/JumpingOnTheClouds.java
+++ src/main/java/hackerrank/JumpingOnTheClouds.java
@@ -6,6 +6,19 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited
  */
 public class JumpingOnTheClouds {
+    private static int jumpingOnTheClouds(int[] c, int k) {
+        int i = 0;
+        int e = 100;
+        do {
+            i = (i + k) % c.length;
+            e--;
+            if (c[i] == 1) {
+                e -= 2;
+            }
+        } while (i != 0);
+        return e;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -14,5 +27,6 @@ public class JumpingOnTheClouds {
         for (int c_i = 0; c_i < n; c_i++) {
             c[c_i] = in.nextInt();
         }
+        System.out.println(jumpingOnTheClouds(c, k));
     }
 }
--- src/test/java/hackerrank/JumpingOnTheCloudsTest.java
+++ src/test/java/hackerrank/JumpingOnTheCloudsTest.java
@@ -7,7 +7,7 @@ import java.io.FileInputStream;
 public class JumpingOnTheCloudsTest {
     @Test
     public void test1() throws Exception {
-        System.setIn(new FileInputStream(""));
+        System.setIn(new FileInputStream("src/test/resources/jumping-on-the-clouds/test1.txt"));
         JumpingOnTheClouds.main(new String[]{});
     }
 }
