f2cb337967ad63315b6e079f2a478fc4ba9b781c
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-06 01:17:24 -0500

    Solve Richie Rich

--- src/test/java/hackerrank/RichieRichTest.java
+++ src/test/java/hackerrank/RichieRichTest.java
@@ -2,9 +2,42 @@ package hackerrank;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+
 public class RichieRichTest {
     @Test
     public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/richie-rich/test1.txt"));
+        RichieRich.main(new String[]{});
+    }
 
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/richie-rich/test2.txt"));
+        RichieRich.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/richie-rich/test3.txt"));
+        RichieRich.main(new String[]{});
+    }
+
+    @Test
+    public void test4() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/richie-rich/test4.txt"));
+        RichieRich.main(new String[]{});
+    }
+
+    @Test
+    public void test5() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/richie-rich/test5.txt"));
+        RichieRich.main(new String[]{});
+    }
+
+    @Test
+    public void test6() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/richie-rich/test6.txt"));
+        RichieRich.main(new String[]{});
     }
 }
