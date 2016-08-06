baa191a54a5deee0c7ac6db238782123c2b79c39
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-06 01:51:31 -0500

    Update String Construction

--- src/main/java/hackerrank/StringConstruction.java
+++ src/main/java/hackerrank/StringConstruction.java
@@ -6,7 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/string-construction
  */
 public class StringConstruction {
+    private static int stringConstruction(String str) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            String str = in.next();
+            System.out.println(stringConstruction(str));
+        }
     }
 }
