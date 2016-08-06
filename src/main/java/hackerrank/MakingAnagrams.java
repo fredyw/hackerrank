f84702daf49b8475d71b48dd78708af367530524
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-06 11:50:55 -0500

    Solve Making Anagrams

--- src/main/java/hackerrank/MakingAnagrams.java
+++ src/main/java/hackerrank/MakingAnagrams.java
@@ -1,5 +1,8 @@
 package hackerrank;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -7,8 +10,51 @@ import java.util.Scanner;
  */
 public class MakingAnagrams {
     private static int makingAnagrams(String a, String b) {
-        // TODO
-        return 0;
+        Map<Character, Integer> mapA = new HashMap<>();
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
+            if (!mapA.containsKey(c)) {
+                mapA.put(c, 1);
+            } else {
+                mapA.put(c, mapA.get(c) + 1);
+            }
+        }
+        Map<Character, Integer> mapB = new HashMap<>();
+        for (int i = 0; i < b.length(); i++) {
+            char c = b.charAt(i);
+            if (!mapB.containsKey(c)) {
+                mapB.put(c, 1);
+            } else{
+                mapB.put(c, mapB.get(c) + 1);
+            }
+        }
+        Iterator<Map.Entry<Character, Integer>> iter = mapA.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Character, Integer> entry = iter.next();
+            char c = entry.getKey();
+            int countA = entry.getValue();
+            if (mapB.containsKey(c)) {
+                int countB = mapB.get(c);
+                if (countA < countB) {
+                    iter.remove();
+                    mapB.put(c, countB - countA);
+                } else if (countA > countB) {
+                    entry.setValue(countA - countB);
+                    mapB.remove(c);
+                } else {
+                    iter.remove();
+                    mapB.remove(c);
+                }
+            }
+        }
+        int count = 0;
+        for (Map.Entry<Character, Integer> e : mapA.entrySet()) {
+            count += e.getValue();
+        }
+        for (Map.Entry<Character, Integer> e : mapB.entrySet()) {
+            count += e.getValue();
+        }
+        return count;
     }
 
     public static void main(String[] args) {
--- src/test/java/hackerrank/MakingAnagramsTest.java
+++ src/test/java/hackerrank/MakingAnagramsTest.java
@@ -7,7 +7,7 @@ import java.io.FileInputStream;
 public class MakingAnagramsTest {
     @Test
     public void test1() throws Exception {
-        System.setIn(new FileInputStream(""));
+        System.setIn(new FileInputStream("src/test/resources/making-anagrams/test1.txt"));
         MakingAnagrams.main(new String[]{});
     }
 }
