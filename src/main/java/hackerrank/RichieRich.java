ea0258cad8206087ff4660d1e2dffe32f4d636cf
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-05 20:47:30 -0500

    Update Richie Rich

--- src/main/java/hackerrank/RichieRich.java
+++ src/main/java/hackerrank/RichieRich.java
@@ -1,10 +1,21 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/richie-rich
  */
 public class RichieRich {
-    public static void main(String[] args) {
+    private static int largestPalindrome(String number) {
+        // TODO:
+        return 0;
+    }
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        String number = in.next();
+        System.out.println(largestPalindrome(number));
     }
 }
