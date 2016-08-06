ccd366e965c62bc39d9c8cc00af315b801a32125
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-06 10:44:18 -0500

    Solve String Construction

--- src/main/java/hackerrank/StringConstruction.java
+++ src/main/java/hackerrank/StringConstruction.java
@@ -1,22 +1,30 @@
 package hackerrank;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * https://www.hackerrank.com/challenges/string-construction
  */
 public class StringConstruction {
     private static int stringConstruction(String str) {
-        // TODO
-        return 0;
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (set.add(str.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            String str = in.next();
-            System.out.println(stringConstruction(str));
+        for (int a0 = 0; a0 < n; a0++) {
+            String s = in.next();
+            System.out.println(stringConstruction(s));
         }
     }
 }
