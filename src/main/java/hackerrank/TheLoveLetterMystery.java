bdd705f68843a795158da2c2170c9f0548ef7deb
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-06 14:18:12 -0500

    Update Love Letter Mystery

--- src/main/java/hackerrank/TheLoveLetterMystery.java
+++ src/main/java/hackerrank/TheLoveLetterMystery.java
@@ -6,7 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/the-love-letter-mystery
  */
 public class TheLoveLetterMystery {
+    private static int loveLetterMystery(String str) {
+        // TODO:
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            String str = in.next();
+            System.out.println(loveLetterMystery(str));
+        }
     }
 }
