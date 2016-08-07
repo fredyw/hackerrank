efc0b914c631bee9de15fb14dc0e126fc00bc705
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-07 16:56:29 -0500

    Solve Caesar Cipher

--- src/main/java/hackerrank/CaesarCipher.java
+++ src/main/java/hackerrank/CaesarCipher.java
@@ -6,8 +6,35 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/caesar-cipher-1
  */
 public class CaesarCipher {
+    private static String caesarCipher(String str, int k) {
+        int newK = k % 26;
+        char[] chars = str.toCharArray();
+        StringBuilder newStr = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'a' && chars[i] <= 'z') {
+                if (chars[i] + newK > 'z') {
+                    newStr.append((char) (chars[i] + newK - 26));
+                } else {
+                    newStr.append((char) (chars[i] + newK));
+                }
+            } else if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                if (chars[i] + newK > 'Z') {
+                    newStr.append((char) (chars[i] + newK - 26));
+                } else {
+                    newStr.append((char) (chars[i] + newK));
+                }
+            } else {
+                newStr.append(chars[i]);
+            }
+        }
+        return newStr.toString();
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int n = in.nextInt();
+        String str = in.next();
+        int k = in.nextInt();
+        System.out.println(caesarCipher(str, k));
     }
 }
--- src/test/java/hackerrank/CaesarCipherTest.java
+++ src/test/java/hackerrank/CaesarCipherTest.java
@@ -10,4 +10,10 @@ public class CaesarCipherTest {
         System.setIn(new FileInputStream("src/test/resources/caesar-cipher/test1.txt"));
         CaesarCipher.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/caesar-cipher/test2.txt"));
+        CaesarCipher.main(new String[]{});
+    }
 }
