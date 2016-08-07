6a1231accd286a6c1895bd5f43801a52b424ad77
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-07 16:37:57 -0500

    Skeleton for Caesar Cipher

---
+++ src/main/java/hackerrank/CaesarCipher.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/caesar-cipher-1
+ */
+public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+    }
+}
---
+++ src/test/java/hackerrank/CaesarCipherTest.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+
+public class CaesarCipherTest {
+    @Test
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/caesar-cipher/test1.txt"));
+        CaesarCipher.main(new String[]{});
+    }
+}
