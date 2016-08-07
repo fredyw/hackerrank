8369135f119a35bb9c970c45fb9340ff4dbe2f1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-07 12:12:31 -0500

    Solve Find the Median

--- src/main/java/hackerrank/FindTheMedian.java
+++ src/main/java/hackerrank/FindTheMedian.java
@@ -1,10 +1,24 @@
 package hackerrank;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/find-the-median
  */
 public class FindTheMedian {
-    public static void main(String[] args) {
+    private static int median(int[] array) {
+        Arrays.sort(array);
+        return array[array.length / 2];
+    }
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = in.nextInt();
+        }
+        System.out.println(median(array));
     }
 }
