9b181dc5e56728cf169015a00bf99d4a9ff98632
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-08 18:00:18 -0500

    Skeleton for Minimum Distances

---
+++ src/main/java/hackerrank/MinimumDistances.java
@@ -0,0 +1,17 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/minimum-distances
+ */
+public class MinimumDistances {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int A[] = new int[n];
+        for (int A_i = 0; A_i < n; A_i++) {
+            A[A_i] = in.nextInt();
+        }
+    }
+}
