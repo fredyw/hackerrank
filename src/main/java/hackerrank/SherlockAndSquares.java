94fc74ef65020736fef0aaf81bd838d94d19ea29
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-08 01:10:29 -0500

    Update Sherlock and Squares

--- src/main/java/hackerrank/SherlockAndSquares.java
+++ src/main/java/hackerrank/SherlockAndSquares.java
@@ -6,8 +6,19 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/sherlock-and-squares
  */
 public class SherlockAndSquares {
+    private static int sherlockAndSquares(int a, int b) {
+        int result = 0;
+        // TODO
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(sherlockAndSquares(a, b));
+        }
     }
 }
