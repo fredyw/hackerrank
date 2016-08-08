91087af9a066e501f3b429fcd32a87c56521055e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-08 00:18:54 -0500

    Skeleton for Fair Rations

---
+++ src/test/java/hackerrank/FairRationsTest.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+
+public class FairRationsTest {
+    @Test
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/fair-rations/test1.txt"));
+        FairRations.main(new String[]{});
+    }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/fair-rations/test2.txt"));
+        FairRations.main(new String[]{});
+    }
+}
