80a75636994a7e52996eff41236ea46fdc568e96
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-08 00:13:48 -0500

    Solve Find Digits

--- src/main/java/hackerrank/FindDigits.java
+++ src/main/java/hackerrank/FindDigits.java
@@ -6,8 +6,26 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/find-digits
  */
 public class FindDigits {
+    private static int findDigits(String str) {
+        int num = Integer.parseInt(str);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int n = Integer.parseInt("" + str.charAt(i));
+            if (n != 0) {
+                if (num % n == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            String str = in.next();
+            System.out.println(findDigits(str));
+        }
     }
 }
