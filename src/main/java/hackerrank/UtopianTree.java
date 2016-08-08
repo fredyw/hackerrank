9b78a089e00cc5e850ddec52243b9d797e3a531a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-08 01:05:07 -0500

    Solve Utopian Tree

--- src/main/java/hackerrank/UtopianTree.java
+++ src/main/java/hackerrank/UtopianTree.java
@@ -6,8 +6,24 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/utopian-tree
  */
 public class UtopianTree {
+    private static int utopianTree(int n) {
+        int height = 1;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                height *= 2;
+            } else {
+                height++;
+            }
+        }
+        return height;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            System.out.println(utopianTree(n));
+        }
     }
 }
