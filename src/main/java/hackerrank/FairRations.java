d6df34847abfa360c16909800db53897b112adea
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-08 00:26:22 -0500

    Update Fair Rations

--- src/main/java/hackerrank/FairRations.java
+++ src/main/java/hackerrank/FairRations.java
@@ -6,8 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/fair-rations
  */
 public class FairRations {
+    private static void fairRations(int[] array) {
+        // TODO
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        fairRations(array);
     }
 }
