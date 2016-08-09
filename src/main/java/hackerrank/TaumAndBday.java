62fd9c05a3a6de917839672df4875e0883f7c811
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-09 10:01:57 -0500

    Skeleton for Taum and B'day

---
+++ src/main/java/hackerrank/TaumAndBday.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/taum-and-bday
+ */
+public class TaumAndBday {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            long b = in.nextLong();
+            long w = in.nextLong();
+            long x = in.nextLong();
+            long y = in.nextLong();
+            long z = in.nextLong();
+        }
+        // TODO
+    }
+}
