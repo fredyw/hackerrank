1f41991ae6d74f4229af562ea539b3ac6f2f768b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-10 14:07:41 -0500

    Skeleton for Flatland Space Station

---
+++ src/main/java/hackerrank/FlatlandSpaceStation.java
@@ -0,0 +1,12 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/flatland-space-stations
+ */
+public class FlatlandSpaceStation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
---
+++ src/test/java/hackerrank/FlatlandSpaceStationTest.java
@@ -0,0 +1,19 @@
+package hackerrank;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+
+public class FlatlandSpaceStationTest {
+    @Test
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/flatland-space-station/test1.txt"));
+        FlatlandSpaceStation.main(new String[]{});
+    }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/flatland-space-station/test2.txt"));
+        FlatlandSpaceStation.main(new String[]{});
+    }
+}
