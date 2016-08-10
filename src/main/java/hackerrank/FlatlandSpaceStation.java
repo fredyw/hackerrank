4e6375cec6e46c1bfe4bdda8c7760f1fc680514b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-10 18:16:32 -0500

    Update Flatland Space Stations

--- src/main/java/hackerrank/FlatlandSpaceStation.java
+++ src/main/java/hackerrank/FlatlandSpaceStation.java
@@ -6,8 +6,20 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/flatland-space-stations
  */
 public class FlatlandSpaceStation {
+    private static int flatlandSpaceStations(int n, int[] stations) {
+        int max = 0;
+        // TODO
+        return max;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO:
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] stations = new int[n];
+        for (int i = 0; i < m; i++) {
+            stations[i] = in.nextInt();
+        }
+        System.out.println(flatlandSpaceStations(n, stations));
     }
 }
