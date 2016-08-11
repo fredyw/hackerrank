82473a735831eff861f463bfcaaf7633b356ffbd
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-10 23:23:44 -0500

    Solve Flatland Space Station

--- src/main/java/hackerrank/FlatlandSpaceStation.java
+++ src/main/java/hackerrank/FlatlandSpaceStation.java
@@ -1,5 +1,6 @@
 package hackerrank;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -7,8 +8,25 @@ import java.util.Scanner;
  */
 public class FlatlandSpaceStation {
     private static int flatlandSpaceStations(int n, int[] stations) {
+        Arrays.sort(stations);
         int max = 0;
-        // TODO
+        int prevIdx = -1;
+        int currIdx = 0;
+        for (int i = 0; i < n; i++) {
+            if (currIdx < stations.length && stations[currIdx] < i) {
+                prevIdx = currIdx;
+                currIdx++;
+            }
+            int prevDiff = Integer.MAX_VALUE;
+            if (prevIdx != -1) {
+                prevDiff = Math.abs(stations[prevIdx] - i);
+            }
+            int currDiff = Integer.MAX_VALUE;
+            if (currIdx < stations.length) {
+                currDiff = Math.abs(stations[currIdx] - i);
+            }
+            max = Math.max(max, Math.min(prevDiff, currDiff));
+        }
         return max;
     }
 
@@ -16,7 +34,7 @@ public class FlatlandSpaceStation {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int m = in.nextInt();
-        int[] stations = new int[n];
+        int[] stations = new int[m];
         for (int i = 0; i < m; i++) {
             stations[i] = in.nextInt();
         }
--- src/test/java/hackerrank/FlatlandSpaceStationTest.java
+++ src/test/java/hackerrank/FlatlandSpaceStationTest.java
@@ -16,4 +16,10 @@ public class FlatlandSpaceStationTest {
         System.setIn(new FileInputStream("src/test/resources/flatland-space-station/test2.txt"));
         FlatlandSpaceStation.main(new String[]{});
     }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/flatland-space-station/test3.txt"));
+        FlatlandSpaceStation.main(new String[]{});
+    }
 }
