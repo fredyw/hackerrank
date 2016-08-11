e7297998dee3f640f7e5687def7ca7c54211d1a4
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-11 00:03:22 -0500

    Solve Library Fine

--- src/main/java/hackerrank/LibraryFine.java
+++ src/main/java/hackerrank/LibraryFine.java
@@ -6,8 +6,35 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/library-fine
  */
 public class LibraryFine {
+    private static int libraryFine(int actualDay, int actualMonth, int actualYear,
+                                   int expectedDay, int expectedMonth, int expectedYear) {
+        if (actualYear < expectedYear) {
+            return 0;
+        }
+        if (actualYear > expectedYear) {
+            return 10000;
+        }
+        if (actualMonth > expectedMonth) {
+            return 500 * (actualMonth - expectedMonth);
+        } else if (actualMonth < expectedDay) {
+            return 0;
+        }
+
+        if (actualDay > expectedDay) {
+            return 15 * (actualDay - expectedDay);
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int actualDay = in.nextInt();
+        int actualMonth = in.nextInt();
+        int actualYear = in.nextInt();
+        int expectedDay = in.nextInt();
+        int expectedMonth = in.nextInt();
+        int expectedYear = in.nextInt();
+        System.out.println(libraryFine(actualDay, actualMonth, actualYear,
+            expectedDay, expectedMonth, expectedYear));
     }
 }
--- src/test/java/hackerrank/LibraryFineTest.java
+++ src/test/java/hackerrank/LibraryFineTest.java
@@ -10,4 +10,22 @@ public class LibraryFineTest {
         System.setIn(new FileInputStream("src/test/resources/library-fine/test1.txt"));
         LibraryFine.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/library-fine/test2.txt"));
+        LibraryFine.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/library-fine/test3.txt"));
+        LibraryFine.main(new String[]{});
+    }
+
+    @Test
+    public void test4() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/library-fine/test4.txt"));
+        LibraryFine.main(new String[]{});
+    }
 }
