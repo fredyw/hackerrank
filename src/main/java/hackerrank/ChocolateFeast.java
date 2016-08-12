470191a145f1ba784d7920df0888c9b9adb8ea86
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-12 00:32:48 -0500

    Update Chocolat Feast

--- src/main/java/hackerrank/ChocolateFeast.java
+++ src/main/java/hackerrank/ChocolateFeast.java
@@ -6,8 +6,18 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/chocolate-feast
  */
 public class ChocolateFeast {
+    private static int chocolateFeast(int n, int m, int c) {
+        // TODO
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int c = in.nextInt();
+            int m = in.nextInt();
+            chocolateFeast(n, m, c);
+        }
     }
 }
