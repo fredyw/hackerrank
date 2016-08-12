c2d7a67671e245e26bdfb61cad28b7c28ce8f0e1
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-12 01:04:49 -0500

    Solve Chocolate Feast

--- src/main/java/hackerrank/ChocolateFeast.java
+++ src/main/java/hackerrank/ChocolateFeast.java
@@ -6,8 +6,16 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/chocolate-feast
  */
 public class ChocolateFeast {
-    private static int chocolateFeast(int n, int m, int c) {
-        // TODO
+    private static int chocolateFeast(int n, int c, int m) {
+        int chocolates = n / c;
+        int wrappers = chocolates;
+        while (wrappers / m > 0) {
+            int unusedWrappers = wrappers % m;
+            int newChocolates = wrappers / m;
+            chocolates += newChocolates;
+            wrappers = newChocolates + unusedWrappers;
+        }
+        return chocolates;
     }
 
     public static void main(String[] args) {
@@ -17,7 +25,7 @@ public class ChocolateFeast {
             int n = in.nextInt();
             int c = in.nextInt();
             int m = in.nextInt();
-            chocolateFeast(n, m, c);
+            System.out.println(chocolateFeast(n, c, m));
         }
     }
 }
