0c9ca6511126ad62576123d3f4c5d633b0161e05
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-13 12:00:07 -0500

    Solve Strange Counter

--- src/main/java/hackerrank/StrangeCounter.java
+++ src/main/java/hackerrank/StrangeCounter.java
@@ -6,9 +6,22 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/strange-code
  */
 public class StrangeCounter {
+    private static long strangeCode(long time) {
+        long t = 1;
+        long step = 3;
+        while (t < time) {
+            t += step;
+            step *= 2;
+        }
+        if (time == t) {
+            return step;
+        }
+        return t - time;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        // TODO
+        long t = in.nextLong();
+        System.out.println(strangeCode(t));
     }
 }
