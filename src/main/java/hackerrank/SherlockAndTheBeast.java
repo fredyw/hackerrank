b9598363cccd5c4e278a0f52937577b4ffc9c85e
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-14 10:23:25 -0500

    Update Sherlock and the Beast

--- src/main/java/hackerrank/SherlockAndTheBeast.java
+++ src/main/java/hackerrank/SherlockAndTheBeast.java
@@ -6,8 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/sherlock-and-the-beast
  */
 public class SherlockAndTheBeast {
+    private static int sherlockAndTheBeast(int n) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            System.out.println(sherlockAndTheBeast(n));
+        }
     }
 }
