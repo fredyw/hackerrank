873f602a555b3e6a5100ceb4b97b9ebd36f5cfdf
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-15 00:51:06 -0500

    Solve Correctness and the Loop Invariant

--- src/main/java/hackerrank/CorrectnessAndTheLoopInvariant.java
+++ src/main/java/hackerrank/CorrectnessAndTheLoopInvariant.java
@@ -10,7 +10,7 @@ public class CorrectnessAndTheLoopInvariant {
         for (int i = 1; i < A.length; i++) {
             int value = A[i];
             int j = i - 1;
-            while (j > 0 && A[j] > value) {
+            while (j >= 0 && A[j] > value) {
                 A[j + 1] = A[j];
                 j = j - 1;
             }
--- src/test/java/hackerrank/CorrectnessAndTheLoopInvariantTest.java
+++ src/test/java/hackerrank/CorrectnessAndTheLoopInvariantTest.java
@@ -10,4 +10,10 @@ public class CorrectnessAndTheLoopInvariantTest {
         System.setIn(new FileInputStream("src/test/resources/correctness-and-the-loop-invariant/test1.txt"));
         CorrectnessAndTheLoopInvariant.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/correctness-and-the-loop-invariant/test2.txt"));
+        CorrectnessAndTheLoopInvariant.main(new String[]{});
+    }
 }
