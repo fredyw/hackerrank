d5780a32c38ee1730146be9acd4494b7998d0e4c
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-15 00:32:07 -0500

    Update Beautiful Pairs

--- src/main/java/hackerrank/BeautifulPairs.java
+++ src/main/java/hackerrank/BeautifulPairs.java
@@ -6,8 +6,22 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/beautiful-pairs
  */
 public class BeautifulPairs {
+    private static int beautifulParis(int[] a, int[] b) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = in.nextInt();
+        }
+        System.out.println(beautifulParis(a, b));
     }
 }
