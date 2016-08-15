b8da24652a3cf156d21bff23bacda73a3ac9a9db
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-15 00:43:56 -0500

    Skeleton for Correctness and the Loop Invariant

---
+++ src/main/java/hackerrank/CorrectnessAndTheLoopInvariant.java
@@ -0,0 +1,39 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/correctness-invariant
+ */
+public class CorrectnessAndTheLoopInvariant {
+    public static void insertionSort(int[] A) {
+        for (int i = 1; i < A.length; i++) {
+            int value = A[i];
+            int j = i - 1;
+            while (j > 0 && A[j] > value) {
+                A[j + 1] = A[j];
+                j = j - 1;
+            }
+            A[j + 1] = value;
+        }
+
+        printArray(A);
+    }
+
+
+    static void printArray(int[] ar) {
+        for (int n : ar) {
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = in.nextInt();
+        }
+        insertionSort(ar);
+    }
+}
