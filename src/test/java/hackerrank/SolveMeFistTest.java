555f5453972d607fa903ee412b61e003ecbd3d93
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-16 00:13:33 -0500

    Update Solve Me First

--- src/test/java/hackerrank/SolveMeFistTest.java
+++ src/test/java/hackerrank/SolveMeFistTest.java
@@ -2,9 +2,12 @@ package hackerrank;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+
 public class SolveMeFistTest {
     @Test
-    public void test() {
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/solve-me-first/test1.txt"));
         SolveMeFist.main(new String[]{});
     }
 }
