8eebff134c83222f1a3988b2b5788855f75ec9d1
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-18 00:06:18 -0500

    Update Quicksort: In-Place

--- src/main/java/hackerrank/QuicksortInPlace.java
+++ src/main/java/hackerrank/QuicksortInPlace.java
@@ -7,7 +7,48 @@ import java.util.Scanner;
  */
 public class QuicksortInPlace {
     private static void quicksort(int[] array) {
-        // TODO
+        quicksort(array, 0, array.length - 1);
+    }
+
+    private static void quicksort(int[] array, int begin, int end) {
+        if (begin > end) {
+            return;
+        }
+        int p = partition(array, begin, end);
+        quicksort(array, begin, p - 1);
+        quicksort(array, p + 1, end);
+    }
+
+    private static int partition(int[] array, int begin, int end) {
+        int pivot = begin;
+        int left = pivot + 1;
+        int right = end;
+        while (true) {
+            while (left <= end && array[left] < array[pivot]) {
+                left++;
+            }
+            if (left > end) {
+                break;
+            }
+            while (right >= begin && array[pivot] < array[right]) {
+                right--;
+            }
+            if (right < begin) {
+                break;
+            }
+            if (left >= right) {
+                break;
+            }
+            swap(array, left, right);
+        }
+        swap(array, pivot, right);
+        return right;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 
     public static void main(String[] args) {
