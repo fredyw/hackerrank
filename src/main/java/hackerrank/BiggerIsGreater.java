c4efb3582f3fd842805728b6fa4f287a402e7230
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-18 23:43:10 -0500

    Update Bigger is Greater

--- src/main/java/hackerrank/BiggerIsGreater.java
+++ src/main/java/hackerrank/BiggerIsGreater.java
@@ -6,8 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/bigger-is-greater
  */
 public class BiggerIsGreater {
+    private static String nextPermutation(String str) {
+        // TODO
+        return null;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            String str = in.next();
+            System.out.println(nextPermutation(str));
+        }
     }
 }
