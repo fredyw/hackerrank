894d838e7de4db82165c36497efcc76d188d5b20
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 03:21:45 -0500

    Solve Encryption

--- src/main/java/hackerrank/Encryption.java
+++ src/main/java/hackerrank/Encryption.java
@@ -6,8 +6,37 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/encryption
  */
 public class Encryption {
+    private static String encryption(String str) {
+        String newStr = str.replaceAll("\\s+", "");
+        double sqrt = Math.sqrt(newStr.length());
+        int row = (int) Math.ceil(sqrt);
+        int col = (int) Math.ceil(sqrt);
+        char[][] chars = new char[row][col];
+        int idx = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (idx == newStr.length()) {
+                    break;
+                }
+                chars[i][j] = newStr.charAt(idx++);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (chars[j][i] == 0) {
+                    continue;
+                }
+                sb.append(chars[j][i]);
+            }
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        String str = in.nextLine();
+        System.out.println(encryption(str));
     }
 }
--- src/test/java/hackerrank/EncryptionTest.java
+++ src/test/java/hackerrank/EncryptionTest.java
@@ -10,4 +10,22 @@ public class EncryptionTest {
         System.setIn(new FileInputStream("src/test/resources/encryption/test1.txt"));
         Encryption.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/encryption/test2.txt"));
+        Encryption.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/encryption/test3.txt"));
+        Encryption.main(new String[]{});
+    }
+
+    @Test
+    public void test4() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/encryption/test4.txt"));
+        Encryption.main(new String[]{});
+    }
 }
