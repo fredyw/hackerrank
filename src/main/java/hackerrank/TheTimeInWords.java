909e6367edde4347aaf6f3fb4b54271d46877c7b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 12:06:01 -0500

    Solve The Time In Words

--- src/main/java/hackerrank/TheTimeInWords.java
+++ src/main/java/hackerrank/TheTimeInWords.java
@@ -1,16 +1,63 @@
 package hackerrank;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
  * https://www.hackerrank.com/challenges/the-time-in-words
  */
 public class TheTimeInWords {
-    private static String timeInWords(int hours, int minutes) {
-        // TODO
-        return null;
+    private static final Map<Integer, String> dict = new HashMap<>();
+    static {
+        dict.put(1, "one");
+        dict.put(2, "two");
+        dict.put(3, "three");
+        dict.put(4, "four");
+        dict.put(5, "five");
+        dict.put(6, "six");
+        dict.put(7, "seven");
+        dict.put(8, "eight");
+        dict.put(9, "nine");
+        dict.put(10, "ten");
+        dict.put(11, "eleven");
+        dict.put(12, "twelve");
+        dict.put(13, "thirteen");
+        dict.put(14, "fourteen");
+        dict.put(15, "fifteen");
+        dict.put(16, "sixteen");
+        dict.put(17, "seventeen");
+        dict.put(18, "eighteen");
+        dict.put(19, "nineteen");
+        dict.put(20, "twenty");
+        for (int i = 1; i <= 9; i++) {
+            dict.put(20 + i, "twenty " + dict.get(i));
+        }
     }
-    
+
+    private static String timeInWords(int hours, int minutes) {
+        if (minutes == 0) {
+            return dict.get(hours) + " o' clock";
+        }
+        if (minutes == 15) {
+            return "quarter past " + dict.get(hours);
+        }
+        if (minutes == 30) {
+            return "half past " + dict.get(hours);
+        }
+        if (minutes == 45) {
+            return "quarter to " + dict.get(hours + 1);
+        }
+        String min = "minute";
+        if (minutes > 1) {
+            min += "s";
+        }
+        if (minutes < 30) {
+            return dict.get(minutes) + " " + min + " past " + dict.get(hours);
+        }
+        return dict.get(60 - minutes) + " " + min + " to " + dict.get(hours + 1);
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int hours = in.nextInt();
--- src/test/java/hackerrank/TheTimeInWordsTest.java
+++ src/test/java/hackerrank/TheTimeInWordsTest.java
@@ -10,4 +10,52 @@ public class TheTimeInWordsTest {
         System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test1.txt"));
         TheTimeInWords.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test2.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test3.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
+
+    @Test
+    public void test4() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test4.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
+
+    @Test
+    public void test5() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test5.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
+
+    @Test
+    public void test6() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test6.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
+
+    @Test
+    public void test7() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test7.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
+
+    @Test
+    public void test8() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test8.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
+
+    @Test
+    public void test9() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-time-in-words/test9.txt"));
+        TheTimeInWords.main(new String[]{});
+    }
 }
