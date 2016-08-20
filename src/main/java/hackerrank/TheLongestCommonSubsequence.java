a9334dc1229cb0e95d8623a00c9f07ff221e8c26
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 02:15:01 -0500

    Update The Longest Common Subsequence

--- src/main/java/hackerrank/TheLongestCommonSubsequence.java
+++ src/main/java/hackerrank/TheLongestCommonSubsequence.java
@@ -1,5 +1,9 @@
 package hackerrank;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -7,7 +11,51 @@ import java.util.Scanner;
  */
 public class TheLongestCommonSubsequence {
     private static void longestCommonSubsequence(int[] array1, int[] array2) {
-        // TODO
+        LCS lcs = longestCommonSubsequence(array1, array2, 0, 0, new HashMap<>());
+        List<Integer> list = lcs.list;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    private static class LCS {
+        private int max;
+        private List<Integer> list = new ArrayList<>();
+
+        private LCS(int max) {
+            this.max = max;
+        }
+    }
+
+    private static LCS longestCommonSubsequence(int[] array1, int[] array2, int i, int j,
+                                                Map<String, LCS> memo) {
+        if (i == array1.length || j == array2.length) {
+            return new LCS(0);
+        }
+        String key = i + "|" + j;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        if (array1[i] == array2[j]) {
+            LCS lcs = longestCommonSubsequence(array1, array2, i + 1, j + 1, memo);
+            lcs.list.add(array1[i]);
+            lcs.max++;
+            memo.put(key, lcs);
+            return lcs;
+        }
+        LCS a = longestCommonSubsequence(array1, array2, i, j + 1, memo);
+        LCS b = longestCommonSubsequence(array1, array2, i + 1, j, memo);
+        if (a.max < b.max) {
+            LCS lcs = new LCS(b.max);
+            lcs.list.addAll(b.list);
+            memo.put(key, lcs);
+            return lcs;
+        }
+        LCS lcs = new LCS(a.max);
+        lcs.list.addAll(a.list);
+        memo.put(key, lcs);
+        return lcs;
     }
 
     public static void main(String[] args) {
--- src/test/java/hackerrank/TheLongestCommonSubsequenceTest.java
+++ src/test/java/hackerrank/TheLongestCommonSubsequenceTest.java
@@ -10,4 +10,10 @@ public class TheLongestCommonSubsequenceTest {
         System.setIn(new FileInputStream("src/test/resources/the-longest-common-subsequence/test1.txt"));
         TheLongestCommonSubsequence.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-longest-common-subsequence/test2.txt"));
+        TheLongestCommonSubsequence.main(new String[]{});
+    }
 }
