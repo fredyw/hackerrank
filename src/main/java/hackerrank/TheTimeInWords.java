7fb29aca65c444f15958ff1fc4b151b2e0084100
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 03:39:44 -0500

    Skeleton for The Time In Words

--- src/main/java/hackerrank/TheTimeInWords.java
+++ src/main/java/hackerrank/TheTimeInWords.java
@@ -6,8 +6,15 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/the-time-in-words
  */
 public class TheTimeInWords {
+    private static String timeInWords(int hours, int minutes) {
+        // TODO
+        return null;
+    }
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int hours = in.nextInt();
+        int minutes = in.nextInt();
+        System.out.println(timeInWords(hours, minutes));
     }
 }
