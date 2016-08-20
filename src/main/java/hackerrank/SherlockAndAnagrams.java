7db5a1ab6a9c5c4d6c10f227c684e311eca4be1d
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 12:56:50 -0500

    Skeleton for Sherlock And Anagrams

--- src/main/java/hackerrank/SherlockAndAnagrams.java
+++ src/main/java/hackerrank/SherlockAndAnagrams.java
@@ -6,8 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/sherlock-and-anagrams
  */
 public class SherlockAndAnagrams {
+    private static int sherlockAndAnagrams(String str) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            String str = in.next();
+            System.out.println(sherlockAndAnagrams(str));
+        }
     }
 }
--- src/test/java/hackerrank/SherlockAndAnagramsTest.java
+++ src/test/java/hackerrank/SherlockAndAnagramsTest.java
@@ -4,6 +4,46 @@ import org.junit.Test;
 
 import java.io.FileInputStream;
 
+// abba
+// a|0,0| a|3,3|
+// b|1,1| b|2,2|
+// ab|0,1| ab|2,3|
+// abb|0,2| abb|1,3|
+// 4
+//
+// abcd
+// 0
+//
+// ifailuhkqq
+// i|0,0| i|3,3|
+// q|8,8| q|9,9|
+// afi|0,2| afi|1,3|
+// 3
+//
+// hucpoltgty
+// t|6,6| t|8,8|
+// gt|6,7| gt|7,8|
+// 2
+//
+// ovarjsnrbf
+// r|3,3| r|7,7|
+// jnrs|3,6| jnrs|4,7|
+// 2
+//
+// pvmupwjjjf
+// p|0,0| p|4,4|
+// j|6,6| j|7,7|
+// j|6,6| j|8,8|
+// j|7,7| j|8,8|
+// jj|6,7| jj|7,8|
+// mpuv|0,3| mpuv|1,4|
+// 6
+//
+// iwwhrlkpek
+// w|1,1| w|2,2|
+// k|6,6| k|9,9|
+// ekp|6,8| ekp|7,9|
+// 3
 public class SherlockAndAnagramsTest {
     @Test
     public void test1() throws Exception {
