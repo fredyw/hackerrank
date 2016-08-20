bf1014c4a6ee5adb712f6ce5370fe650382a4d3c
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 01:29:36 -0500

    Update Longest Common Subsequence

--- src/main/java/hackerrank/TheLongestCommonSubsequence.java
+++ src/main/java/hackerrank/TheLongestCommonSubsequence.java
@@ -6,8 +6,22 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/dynamic-programming-classics-the-longest-common-subsequence
  */
 public class TheLongestCommonSubsequence {
+    private static void longestCommonSubsequence(int[] array1, int[] array2) {
+        // TODO
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] array1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            array1[i] = in.nextInt();
+        }
+        int[] array2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            array2[i] = in.nextInt();
+        }
+        longestCommonSubsequence(array1, array2);
     }
 }
