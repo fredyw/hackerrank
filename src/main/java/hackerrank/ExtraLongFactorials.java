301470272cb02381c478e8236f0434ae9a847b78
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 03:24:11 -0500

    Skeleton for Extra Long Factorials

---
+++ src/main/java/hackerrank/ExtraLongFactorials.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/extra-long-factorials
+ */
+public class ExtraLongFactorials {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // TODO
+    }
+}
