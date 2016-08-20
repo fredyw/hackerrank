49f41047eabc752254f16d69bed9489d158fa133
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 03:37:33 -0500

    Skeleton for The Time In Words

---
+++ src/main/java/hackerrank/TheTimeInWords.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/the-time-in-words
+ */
+public class TheTimeInWords {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // TODO
+    }
+}
