daf0a45329eec4e410c416fb2cd2dd3b62d7a18a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 03:34:53 -0500

    Solve Extra Long Factorials

--- src/main/java/hackerrank/ExtraLongFactorials.java
+++ src/main/java/hackerrank/ExtraLongFactorials.java
@@ -1,13 +1,22 @@
 package hackerrank;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
  * https://www.hackerrank.com/challenges/extra-long-factorials
  */
 public class ExtraLongFactorials {
+    private static BigInteger factorial(int n) {
+        if (n == 1 || n == 2) {
+            return BigInteger.valueOf(n);
+        }
+        return factorial(n - 1).multiply(BigInteger.valueOf(n));
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        System.out.println(factorial(n));
     }
 }
