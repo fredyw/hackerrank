eee65791c315bafdf7c1dcefa07b8c31f5bacd38
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-20 01:14:55 -0500

    Skeleton for The Longest Common Subsequence

---
+++ src/main/java/hackerrank/TheLongestCommonSubsequence.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/dynamic-programming-classics-the-longest-common-subsequence
+ */
+public class TheLongestCommonSubsequence {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // TODO
+    }
+}
---
+++ src/test/java/hackerrank/TheLongestCommonSubsequenceTest.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+
+public class TheLongestCommonSubsequenceTest {
+    @Test
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/the-longest-common-subsequence/test1.txt"));
+        TheLongestCommonSubsequence.main(new String[]{});
+    }
+}
