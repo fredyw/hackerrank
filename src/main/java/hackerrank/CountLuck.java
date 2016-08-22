fb311106d870eec5d6c6be5f5f41981cffe28539
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-22 18:14:46 -0500

    Skeleton for Count Luck

---
+++ src/main/java/hackerrank/CountLuck.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/count-luck
+ */
+public class CountLuck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // TODO
+    }
+}
---
+++ src/test/java/hackerrank/CountLuckTest.java
@@ -0,0 +1,13 @@
+package hackerrank;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+
+public class CountLuckTest {
+    @Test
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/count-luck/test1.txt"));
+        CountLuck.main(new String[]{});
+    }
+}
