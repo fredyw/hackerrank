5105510bc145b65c16fa1fbc7ae760a936724457
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-21 23:40:04 -0500

    Update Sherlock And Anagrams

--- src/main/java/hackerrank/SherlockAndAnagrams.java
+++ src/main/java/hackerrank/SherlockAndAnagrams.java
@@ -1,5 +1,9 @@
 package hackerrank;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -7,7 +11,29 @@ import java.util.Scanner;
  */
 public class SherlockAndAnagrams {
     private static int sherlockAndAnagrams(String str) {
-        // TODO
+        Map<Character, List<Integer>> charIndices = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!charIndices.containsKey(c)) {
+                List<Integer> newList = new LinkedList<>();
+                newList.add(i);
+                charIndices.put(c, newList);
+            } else {
+                charIndices.get(c).add(i);
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (charIndices.containsKey(c)) {
+                List<Integer> indices = charIndices.get(c);
+                for (int idx : indices) {
+                    if (i == idx) {
+                        indices.remove(i);
+                    }
+                }
+            }
+        }
+
         return 0;
     }
 
@@ -18,5 +44,15 @@ public class SherlockAndAnagrams {
             String str = in.next();
             System.out.println(sherlockAndAnagrams(str));
         }
+//        int count = 0;
+//        int n = 3;
+//        for (int step = 1; step < n; step++) {
+//            for (int i = 0; i < n - step + 1; i += step) {
+//                for (int j = i + 1; j < n - step + 1; j += step) {
+//                    count++;
+//                }
+//            }
+//        }
+//        System.out.println(count);
     }
 }
