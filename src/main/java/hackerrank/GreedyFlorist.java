35822b0c851ece29c421eb599d6fe213747f15b8
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-23 15:39:17 -0500

    Update Greedy Florist

--- src/main/java/hackerrank/GreedyFlorist.java
+++ src/main/java/hackerrank/GreedyFlorist.java
@@ -6,8 +6,19 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/greedy-florist
  */
 public class GreedyFlorist {
+    private static int greedyFlorist(int[] costs, int k) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] costs = new int[n];
+        for (int i = 0; i < n; i++) {
+            costs[i] = in.nextInt();
+        }
+        System.out.println(greedyFlorist(costs, k));
     }
 }
