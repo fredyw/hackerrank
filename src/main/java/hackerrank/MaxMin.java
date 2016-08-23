36168707a21c8a1b721d89754f0ffc5ba0d22f19
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-23 16:42:28 -0500

    Update Max Min

--- src/main/java/hackerrank/MaxMin.java
+++ src/main/java/hackerrank/MaxMin.java
@@ -6,8 +6,19 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/angry-children
  */
 public class MaxMin {
+    private static int maxMin(int[] array, int k) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        System.out.println(maxMin(array, k));
     }
 }
