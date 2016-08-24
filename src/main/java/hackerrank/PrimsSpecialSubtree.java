31326716e95c952d31f7f9c4ca0f900e3bc8c65b
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-24 13:26:18 -0500

    Solve Prim's; Special Subtree

--- src/main/java/hackerrank/PrimsSpecialSubtree.java
+++ src/main/java/hackerrank/PrimsSpecialSubtree.java
@@ -2,20 +2,55 @@ package hackerrank;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * https://www.hackerrank.com/challenges/primsmstsub
  */
 public class PrimsSpecialSubtree {
-    private static int mst(Map<Integer, List<Edge>> adjList, int start) {
-        // TODO
-        return 0;
+    private static int mst(Map<Integer, List<Edge>> adjList, int source) {
+        PriorityQueue<Edge> queue = new PriorityQueue<>();
+        Set<Integer> marked = new HashSet<>();
+        List<Edge> mst = new ArrayList<>();
+        visit(adjList, source, marked, queue);
+        while (!queue.isEmpty()) {
+            Edge edge = queue.remove();
+            int v1 = edge.either();
+            int v2 = edge.other(v1);
+            if (marked.contains(v1) && marked.contains(v2)) {
+                continue;
+            }
+            mst.add(edge);
+            if (!marked.contains(v1)) {
+                visit(adjList, v1, marked, queue);
+            }
+            if (!marked.contains(v2)) {
+                visit(adjList, v2, marked, queue);
+            }
+        }
+        int totalWeight = 0;
+        for (Edge edge : mst) {
+            totalWeight += edge.weight;
+        }
+        return totalWeight;
     }
 
-    private static class Edge {
+    private static void visit(Map<Integer, List<Edge>> adjList, int source,
+                              Set<Integer> marked, PriorityQueue<Edge> queue) {
+        marked.add(source);
+        for (Edge edge : adjList.get(source)) {
+            if (!marked.contains(edge.other(source))) {
+                queue.add(edge);
+            }
+        }
+    }
+
+    private static class Edge implements Comparable<Edge> {
         private final int v1;
         private final int v2;
         private final int weight;
@@ -26,7 +61,7 @@ public class PrimsSpecialSubtree {
             this.weight = weight;
         }
 
-        public int either(int v) {
+        public int either() {
             return v1;
         }
 
@@ -36,6 +71,11 @@ public class PrimsSpecialSubtree {
             }
             return v1;
         }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(weight, o.weight);
+        }
     }
 
     public static void main(String[] args) {
@@ -43,7 +83,7 @@ public class PrimsSpecialSubtree {
         int n = in.nextInt();
         int m = in.nextInt();
         Map<Integer, List<Edge>> adjList = new HashMap<>();
-        for (int vertex = 0; vertex < n; vertex++) {
+        for (int vertex = 1; vertex <= n; vertex++) {
             adjList.put(vertex, new ArrayList<>());
         }
         for (int i = 0; i < m; i++) {
