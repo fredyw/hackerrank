1d9399cfdc30de1fc5098ef6e659787f184281f2
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-25 12:09:20 -0500

    Update Lego Blocks

--- src/main/java/hackerrank/LegoBlocks.java
+++ src/main/java/hackerrank/LegoBlocks.java
@@ -6,8 +6,18 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/lego-blocks
  */
 public class LegoBlocks {
+    private static int legoBlocks(int n, int m) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            System.out.println(legoBlocks(n, m));
+        }
     }
 }
