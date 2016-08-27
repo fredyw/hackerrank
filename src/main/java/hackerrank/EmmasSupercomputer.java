abf415f2522d376dd193c7730bba2f2ace05f900
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-26 20:04:43 -0500

    Update Emma's Supercomputer

--- src/main/java/hackerrank/EmmasSupercomputer.java
+++ src/main/java/hackerrank/EmmasSupercomputer.java
@@ -6,8 +6,19 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/two-pluses
  */
 public class EmmasSupercomputer {
+    private static int emmasSupercomputer(char[][] matrix, int maxRow, int maxCol) {
+        // TODO:
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int m = in.nextInt();
+        char[][] matrix = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            matrix[i] = in.next().toCharArray();
+        }
+        System.out.println(emmasSupercomputer(matrix, n, m));
     }
 }
