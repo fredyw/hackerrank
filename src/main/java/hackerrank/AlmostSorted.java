a6c3298b304c4e3439e364bfe70439a6e2f2175a
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-27 12:49:56 -0500

    Update Almost Sorted

--- src/main/java/hackerrank/AlmostSorted.java
+++ src/main/java/hackerrank/AlmostSorted.java
@@ -6,8 +6,14 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/almost-sorted
  */
 public class AlmostSorted {
+    private static void almostSorted(int[] array) {
+        // TODO
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int[] array = new int[n];
+        almostSorted(array);
     }
 }
