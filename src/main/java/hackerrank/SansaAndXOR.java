7328e6a2628a5019b74aec7d3fec78d710de2edc
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-29 11:11:59 -0500

    Update Sansa and XOR

--- src/main/java/hackerrank/SansaAndXOR.java
+++ src/main/java/hackerrank/SansaAndXOR.java
@@ -6,8 +6,18 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/sansa-and-xor
  */
 public class SansaAndXOR {
+    private static int sansaAndXOR(int[] array) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int[] array = new int[n];
+            System.out.println(sansaAndXOR(array));
+        }
     }
 }
