c20741a7f64fa5a324584c4bd42f298fcfb9c99f
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-29 21:19:08 -0500

    Update The Bomberman Game

--- src/main/java/hackerrank/TheBombermanGame.java
+++ src/main/java/hackerrank/TheBombermanGame.java
@@ -6,8 +6,19 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/bomber-man
  */
 public class TheBombermanGame {
+    private static void bomberman(char[][] matrix, int maxRow, int maxCol, int seconds) {
+        // TODO
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int r = in.nextInt();
+        int c = in.nextInt();
+        int n = in.nextInt();
+        char[][] matrix = new char[r][c];
+        for (int i = 0; i < r; i++) {
+            matrix[i] = in.next().toCharArray();
+        }
+        bomberman(matrix, r, c, n);
     }
 }
