84c5e34c9b9833231d2bed298cea0333438bd247
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-30 22:42:11 -0500

    Update Absolute Permutation

--- src/main/java/hackerrank/AbsolutePermutation.java
+++ src/main/java/hackerrank/AbsolutePermutation.java
@@ -1,13 +1,61 @@
 package hackerrank;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * https://www.hackerrank.com/challenges/absolute-permutation
  */
 public class AbsolutePermutation {
     private static void absolutePermutation(int n, int k) {
-        // TODO:
+        List<Integer> result = absolutePermutation(n, k, 1, new ArrayList<>(), new HashSet<>());
+        if (result.isEmpty()) {
+            System.out.println("-1");
+        } else {
+            for (int num : result) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        // TODO
+    }
+
+    private static List<Integer> absolutePermutation(int n, int k, int i, List<Integer> accu,
+                                            Set<Integer> set) {
+        if (i == n + 1) {
+            if (accu.size() == n) {
+                return accu;
+            }
+            return new ArrayList<>();
+        }
+        if (i + k <= n) {
+            List<Integer> newAccu = new ArrayList<>(accu);
+            newAccu.add(i + k);
+            Set<Integer> newSet = new HashSet<>(set);
+            if (!newSet.add(i + k)) {
+                return new ArrayList<>();
+            }
+            List<Integer> result = absolutePermutation(n, k, i + 1, newAccu, newSet);
+            if (!result.isEmpty()) {
+                return result;
+            }
+        }
+        if (i - k >= 1) {
+            List<Integer> newAccu = new ArrayList<>(accu);
+            newAccu.add(i - k);
+            Set<Integer> newSet = new HashSet<>(set);
+            if (!newSet.add(i - k)) {
+                return new ArrayList<>();
+            }
+            List<Integer> result = absolutePermutation(n, k, i + 1, newAccu, newSet);
+            if (!result.isEmpty()) {
+                return result;
+            }
+        }
+        return new ArrayList<>();
     }
 
     public static void main(String[] args) {
