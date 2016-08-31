3477f9c19d583d38fe897e5817aeda209575bcd4
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-30 23:50:03 -0500

    Update Candies

--- src/main/java/hackerrank/Candies.java
+++ src/main/java/hackerrank/Candies.java
@@ -6,8 +6,18 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/candies
  */
 public class Candies {
+    private static int candies(int[] ratings) {
+        // TODO
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int[] ratings = new int[n];
+        for (int i = 0; i < n; i++) {
+            ratings[i] = in.nextInt();
+        }
+        System.out.println(candies(ratings));
     }
 }
