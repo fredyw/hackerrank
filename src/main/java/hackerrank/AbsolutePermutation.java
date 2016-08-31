8760c876797e2446f43d8023010bdd5e0e24b186
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-08-30 22:06:08 -0500

    Update Absolute Permutation

--- src/main/java/hackerrank/AbsolutePermutation.java
+++ src/main/java/hackerrank/AbsolutePermutation.java
@@ -6,8 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/absolute-permutation
  */
 public class AbsolutePermutation {
+    private static void absolutePermutation(int n, int k) {
+        // TODO:
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int testCases = in.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            absolutePermutation(n, k);
+        }
     }
 }
