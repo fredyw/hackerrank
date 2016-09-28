d10dc90ac1f059ccf3520c960dea363807a95008
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-28 09:40:04 -0500

    Skeleton for A Tale of TWo Stacks

---
+++ src/main/java/hackerrank/ctci/ATaleOfTwoStacks.java
@@ -0,0 +1,13 @@
+package hackerrank.ctci;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks
+ */
+public class ATaleOfTwoStacks {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // TODO
+    }
+}
---
+++ src/test/java/hackerrank/ctci/ATaleOfTwoStacksTest.java
@@ -0,0 +1,13 @@
+package hackerrank.ctci;
+
+import org.junit.Test;
+
+import java.io.FileInputStream;
+
+public class ATaleOfTwoStacksTest {
+    @Test
+    public void test1() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/ctci/a-tale-of-two-stacks/test1.txt"));
+        ATaleOfTwoStacks.main(new String[]{});
+    }
+}
