48eb586f76aaef5c919ad0c77dd48610877d063c
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-28 18:47:37 -0500

    Solve Ice Cream Parlor

--- src/main/java/hackerrank/ctci/IceCreamParlor.java
+++ src/main/java/hackerrank/ctci/IceCreamParlor.java
@@ -1,11 +1,43 @@
 package hackerrank.ctci;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 /**
  * https://www.hackerrank.com/challenges/ctci-ice-cream-parlor
  */
 public class IceCreamParlor {
+    private static void iceCream(int[] array, int money) {
+        Map<Integer, TreeSet<Integer>> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (!map.containsKey(array[i])) {
+                TreeSet<Integer> set = new TreeSet<>();
+                set.add(i + 1);
+                map.put(array[i], set);
+            } else {
+                map.get(array[i]).add(i + 1);
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            int diff = money - array[i];
+            if (map.containsKey(diff)) {
+                TreeSet<Integer> set = map.get(diff);
+                if (diff == array[i]) {
+                    if (set.size() >= 2) {
+                        System.out.println(set.pollFirst() + " " + set.pollFirst());
+                        return;
+                    }
+                } else {
+                    int idx = set.pollFirst();
+                    System.out.println(Math.min(idx, i + 1) + " " + Math.max(idx, i + 1));
+                    return;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
@@ -16,7 +48,7 @@ public class IceCreamParlor {
             for (int a_i = 0; a_i < n; a_i++) {
                 a[a_i] = in.nextInt();
             }
+            iceCream(a, m);
         }
-        // TODO
     }
 }
--- src/test/java/hackerrank/ctci/IceCreamParlorTest.java
+++ src/test/java/hackerrank/ctci/IceCreamParlorTest.java
@@ -10,4 +10,10 @@ public class IceCreamParlorTest {
         System.setIn(new FileInputStream("src/test/resources/ctci/ice-cream-parlor/test1.txt"));
         IceCreamParlor.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/ctci/ice-cream-parlor/test2.txt"));
+        IceCreamParlor.main(new String[]{});
+    }
 }
