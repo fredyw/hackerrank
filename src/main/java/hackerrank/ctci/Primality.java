38a519b26ce7266177efd66dddacef8bb2030d53
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-28 17:17:39 -0500

    Solve Primality

--- src/main/java/hackerrank/ctci/Primality.java
+++ src/main/java/hackerrank/ctci/Primality.java
@@ -7,8 +7,17 @@ import java.util.Scanner;
  */
 public class Primality {
     private static String isPrime(int n) {
-        // TODO
-        return "";
+        if (n == 1) {
+            return "Not prime";
+        }
+        boolean prime = true;
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return (prime) ? "Prime" : "Not prime";
     }
 
     public static void main(String[] args) {
