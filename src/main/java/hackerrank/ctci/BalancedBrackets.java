d1371a2d43c9451efca9e19f72500d0dcc467395
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-28 01:08:51 -0500

    Solve Balanced Brackets

--- src/main/java/hackerrank/ctci/BalancedBrackets.java
+++ src/main/java/hackerrank/ctci/BalancedBrackets.java
@@ -1,14 +1,35 @@
 package hackerrank.ctci;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * https://www.hackerrank.com/challenges/ctci-balanced-brackets
  */
 public class BalancedBrackets {
     private static String isBalanced(String expression) {
-        // TODO
-        return "NO";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == '{' || c == '(' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return "NO";
+                }
+                char pop = stack.pop();
+                if (pop == '{' && c != '}') {
+                    return "NO";
+                }
+                if (pop == '(' && c != ')') {
+                    return "NO";
+                }
+                if (pop == '[' && c != ']') {
+                    return "NO";
+                }
+            }
+        }
+        return stack.isEmpty() ? "YES" : "NO";
     }
 
     public static void main(String[] args) {
--- src/test/java/hackerrank/ctci/BalancedBracketsTest.java
+++ src/test/java/hackerrank/ctci/BalancedBracketsTest.java
@@ -10,4 +10,10 @@ public class BalancedBracketsTest {
         System.setIn(new FileInputStream("src/test/resources/ctci/balanced-brackets/test1.txt"));
         BalancedBrackets.main(new String[]{});
     }
+
+    @Test
+    public void test2() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/ctci/balanced-brackets/test2.txt"));
+        BalancedBrackets.main(new String[]{});
+    }
 }
