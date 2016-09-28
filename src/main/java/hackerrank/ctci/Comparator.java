12a87c7f58f3d56e2a03c5c51ba4246fe603b02c
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-28 15:50:13 -0500

    Solve Comparator

--- src/main/java/hackerrank/ctci/Comparator.java
+++ src/main/java/hackerrank/ctci/Comparator.java
@@ -1,13 +1,47 @@
 package hackerrank.ctci;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * https://www.hackerrank.com/challenges/ctci-comparator-sorting
  */
 public class Comparator {
+    private static class Checker implements java.util.Comparator<Player> {
+        @Override
+        public int compare(Player o1, Player o2) {
+            if (o1.score == o2.score) {
+                return o1.name.compareTo(o2.name);
+            }
+            return Integer.compare(o2.score, o1.score);
+        }
+    }
+
+    private static class Player{
+        String name;
+        int score;
+
+        Player(String name, int score){
+            this.name = name;
+            this.score = score;
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        // TODO
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Player[] player = new Player[n];
+        Checker checker = new Checker();
+
+        for (int i = 0; i < n; i++) {
+            player[i] = new Player(scan.next(), scan.nextInt());
+        }
+        scan.close();
+
+        Arrays.sort(player, checker);
+        for (int i = 0; i < player.length; i++) {
+            System.out.printf("%s %s\n", player[i].name, player[i].score);
+        }
     }
 }
