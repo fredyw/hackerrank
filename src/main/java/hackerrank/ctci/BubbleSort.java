ef2fb1d8b172457a8011f205f83a8349e24bf972
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-28 15:30:17 -0500

    Solve Bubble Sort

--- src/main/java/hackerrank/ctci/BubbleSort.java
+++ src/main/java/hackerrank/ctci/BubbleSort.java
@@ -6,8 +6,41 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/ctci-bubble-sort
  */
 public class BubbleSort {
+    private static void bubbleSort(int[] a) {
+        int numberOfSwaps = 0;
+        for (int i = 0; i < a.length; i++) {
+            // Track number of elements swapped during a single array traversal
+            for (int j = 0; j < a.length - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                    numberOfSwaps++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        System.out.println(String.format("Array is sorted in %d swaps.", numberOfSwaps));
+        System.out.println(String.format("First Element: %d", a[0]));
+        System.out.println(String.format("Last Element: %d", a[a.length - 1]));
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        bubbleSort(a);
     }
 }
