d323f14556062d8739411edfecfd38c61bc3f7c5
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-09-28 17:01:00 -0500

    Solve Coin Change

--- src/main/java/hackerrank/ctci/CoinChange.java
+++ src/main/java/hackerrank/ctci/CoinChange.java
@@ -1,14 +1,37 @@
 package hackerrank.ctci;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
  * https://www.hackerrank.com/challenges/ctci-coin-change
  */
 public class CoinChange {
-    private static int coinChange(int[] coins, int n) {
-        // TODO
-        return 0;
+    private static long coinChange(int[] coins, int n) {
+        return coinChange(coins, n, 0, new HashMap<>());
+    }
+
+    private static long coinChange(int[] coins, int n, int prev, Map<String, Long> memo) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        String key = n + "|" + prev;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        long total = 0;
+        for (int i = 0; i < coins.length; i++) {
+            if (prev > coins[i]) {
+                continue;
+            }
+            total += coinChange(coins, n - coins[i], coins[i], memo);
+        }
+        memo.put(key, total);
+        return total;
     }
 
     public static void main(String[] args) {
@@ -19,5 +42,6 @@ public class CoinChange {
         for (int coins_i = 0; coins_i < m; coins_i++) {
             coins[coins_i] = in.nextInt();
         }
+        System.out.println(coinChange(coins, n));
     }
 }
--- src/test/java/hackerrank/ctci/CoinChangeTest.java
+++ src/test/java/hackerrank/ctci/CoinChangeTest.java
@@ -13,7 +13,13 @@ public class CoinChangeTest {
 
     @Test
     public void test2() throws Exception {
-        System.setIn(new FileInputStream("src/test/resources/ctci/coin-change/test12.txt"));
+        System.setIn(new FileInputStream("src/test/resources/ctci/coin-change/test2.txt"));
+        CoinChange.main(new String[]{});
+    }
+
+    @Test
+    public void test3() throws Exception {
+        System.setIn(new FileInputStream("src/test/resources/ctci/coin-change/test3.txt"));
         CoinChange.main(new String[]{});
     }
 }
