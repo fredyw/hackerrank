edf43abee967acb201f17a463206ddce14836dad
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-10-03 17:58:17 -0500

    Update Maximum Perimeter Triangle

--- src/main/java/hackerrank/MaximumPerimeterTriangle.java
+++ src/main/java/hackerrank/MaximumPerimeterTriangle.java
@@ -6,8 +6,17 @@ import java.util.Scanner;
  * https://www.hackerrank.com/challenges/maximum-perimeter-triangle
  */
 public class MaximumPerimeterTriangle {
+    private static void maxPerimeterTriangle(int[] array) {
+        // TODO
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        // TODO
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = in.nextInt();
+        }
+        maxPerimeterTriangle(array);
     }
 }
